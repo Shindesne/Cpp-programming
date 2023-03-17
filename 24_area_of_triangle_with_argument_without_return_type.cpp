43e02ca39f5ad5fdd92a32ed58b2a34a8404b4e4
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 23:21:32 +0530

    Create 24_area_of_triangle_with_argument_without_return_type.cpp

---
+++ 24_area_of_triangle_with_argument_without_return_type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+void area()
+{
+    float height,length,a;
+    cout<<"enter the value";
+    cin>>height>>length;
+    a=0.5*height*length;
+    cout<<"area of triangle="<<a;
+}
+int main()
+{
+    area();
+}
