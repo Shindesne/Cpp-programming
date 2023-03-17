eb57433f70e6f69bbace946de2d889b1ee9b92c6
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 21:28:08 +0530

    Create 10_leap_year.cpp

---
+++ 10_leap_year.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+    cout<<"enter the value";
+    cin>>no;
+    if(no%4==0)
+    {
+        cout<<"it is leap year";
+    }
+    else
+    {
+        cout<<"it is not  leap year";
+    }
+}
