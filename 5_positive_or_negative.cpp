4071b4dc669ef8066e642876415e6daa9cce920b
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 16:42:28 +0530

    Create 5_positive_or_negative.cpp

---
+++ 5_positive_or_negative.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+    cout<<("enter the value");
+    cin>>no;
+    if (no>0)
+    {
+      cout<< ("no is positive");
+    }
+    else
+    {
+      cout<<"no is negative";
+    }
+}
