90af5d4c9dadfb7b0d326f8bb34e76db2490526a
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 21:33:34 +0530

    Create Table.cpp

---
+++ Table.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no,i,t;
+    cout<<"enter the no";
+    cin>>no;
+    for(i=1;i<=10;i++)
+    {
+        cout<<no*i<<endl;
+    }
+}
