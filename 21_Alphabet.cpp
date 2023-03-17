5a72270a4de1c6762299099bf1f95add988313ca
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 22:02:44 +0530

    Create 21_Alphabet.cpp

---
+++ 21_Alphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i,j;
+   char ch='a';
+  for(i=1;i<=7;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        cout<<ch;
+        ch++;
+    }
+    cout<<endl;
+    }
+}
