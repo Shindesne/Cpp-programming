f3d96c0de144814915b549681d4e92d51ab730e5
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 21:27:13 +0530

    Create 9_swapping_of_two_no.cpp

---
+++ 9_swapping_of_two_no.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=1,b=2;
+    cout<<"enter the no";
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"a="<<a<<endl<<"b="<<b;
+}
