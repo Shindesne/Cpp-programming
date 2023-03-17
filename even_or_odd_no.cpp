aadad1c2af91f618055fd28987b8a27921a90576
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 21:25:17 +0530

    Create even_or_odd_no.cpp

---
+++ even_or_odd_no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int no;
+    cout<< ("enter the value");
+    cin>>no;
+   if (no%2==0)
+   {
+      cout<< ("no is even");
+   }
+  else
+   {
+       cout<<"no is odd";
+   }
+}
