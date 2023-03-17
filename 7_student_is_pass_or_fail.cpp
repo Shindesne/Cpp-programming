73165de19000e7fcd8da113d9b5bbb370c928309
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-03-17 16:55:22 +0530

    Create 7_student_is_pass_or_fail.cpp

---
+++ 7_student_is_pass_or_fail.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    cout<< ("enter the value");
+    cin>>a>>b>>c;
+    if (a>=35&&b>=35&&c>=35)
+    {
+        cout<<"student is pass";
+    }
+   else
+   {
+       cout<<"Student is fail";
+   }
+}
