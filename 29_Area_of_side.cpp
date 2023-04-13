bd9d1ae537e01189a86eb4d42f44282b8296f07e
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-04-13 23:36:08 +0530

    Create 29_Area_of_side.cpp

---
+++ 29_Area_of_side.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+void sq(int s)
+{
+    int a;
+    a=s*s;
+    cout<<"area="<<a;
+}
+int main()
+{
+    int side;
+    cout<<"enter the side";
+    cin>>side;
+    sq(side);
+}
