e38f270913da64b5a761e11b8f1e3879df66ae76
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-04-13 23:40:07 +0530

    Create 31_area_of_rectangle_using_class.cpp

---
+++ 31_area_of_rectangle_using_class.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+class rect
+{
+    public:
+    int l,b,a;
+    void setdata();
+    void area();
+};
+     void rect::setdata()
+     {
+         cout<<"enter the no";
+         cin>>l>>b;
+     }
+     void rect::area()
+     {
+         a=l*b;
+         cout<<a;
+     }
+     int main()
+     {
+         rect snehal;
+         snehal.setdata();
+         snehal.area();
+     }
+     
