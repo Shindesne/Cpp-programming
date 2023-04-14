e5555439f6fc89fd015437b0d0cbefb12cccba29
Snehal Shinde <90593105+Shindesne@users.noreply.github.com>
2023-04-15 00:06:45 +0530

    Create 43_Display_Multiplication_Table_up_to_10.cpp

---
+++ 43_Display_Multiplication_Table_up_to_10.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // run a loop from 1 to 10
+    // print the multiplication table
+    for (int i = 1; i <= 10; ++i) {
+        cout << n << " * " << i << " = " << n * i << endl;
+    }
+    
+    return 0;
+}
+Display_Multiplication_Table_up_to_10.cpp
